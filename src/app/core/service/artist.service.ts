3a17ef7202d4db99426eb8ebdaf3bdb55fe12579
piyushjain-vw <126873300+piyushjain-vw@users.noreply.github.com>
2026-02-12 11:41:50 +0530

    added user location

--- src/app/core/service/artist.service.ts
+++ src/app/core/service/artist.service.ts
@@ -28,6 +28,9 @@ export interface ArtistFollower {
     userName: string;
     userEmail: string;
     userPhotoUrl?: string;
+    birthDate?: string;
+    latitude?: number;
+    longitude?: number;
     followedAt: string;
 }
 
